bf5d999157572252ad01188ab2522f2fccdd17da
Edouard Carvalho <me@ceduth.dev>
2024-10-05 07:46:05 +0000

    saving latest changes

--- src/flask_oneauth/views/oauth.py
+++ src/flask_oneauth/views/oauth.py
@@ -1,19 +1,24 @@
 from flask import redirect, url_for, current_app
 
-from flask_oneauth import helpers
+from flask_oneauth import helpers, ImproperlyConfigured
 
 
-@current_app.route("/<provider>")
-def facebook_oauth(provider):
-    proxy = helpers.import_attr(f"flask_dance.contrib.{provider}.{provider}")
-    if not proxy.authorized:
-        return redirect(url_for("facebook.login"))
-    resp = proxy.get("/user")
+@current_app.route("/<name>")
+def provider_oauth(name):
+    provider = validate_provider(name)
+    if not provider.authorized:
+        return redirect(url_for(f"{name}.login"))
+    resp = provider.get("/user")
     assert resp.ok
-    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return "You are @{login} on {provider}"\
+        .format(provider=name, login=resp.json()["login"])
 
 
-
-
-
-
+def validate_provider(name, raise_exc=True):
+    try:
+        provider = helpers.import_attr(f"flask_dance.contrib.{name}.{name}")
+    except ImportError as e:
+        if raise_exc:
+            raise ImproperlyConfigured(f"No such provider {name}")
+        provider = None
+    return provider
